992c4cb6049174e6db9d5b9032d682b825c55983
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-07-22 08:47:09 +0600

    feat: calculate results

--- mathparser/mathparser.go
+++ mathparser/mathparser.go
@@ -1,16 +1,18 @@
 package mathparser
 
 import (
+	"fmt"
 	"log"
+	"strconv"
 )
 
 type TokenType string
 
-type Operation struct {
-	Val    string
-	Left   int
-	Right  int
-	Action string
+type Equation struct {
+	Result int
+	Left   Token
+	Right  Token
+	Action Token
 }
 
 type Token struct {
@@ -18,45 +20,69 @@ type Token struct {
 	Literal string
 }
 
-var (
-	operationFns = map[string]func(l, r int) int{}
-	numbersTable = map[string]int{
-		"0": 0,
-		"1": 1,
-		"2": 2,
-		"3": 3,
-		"4": 4,
-		"5": 5,
-		"6": 6,
-		"7": 7,
-		"8": 8,
-		"9": 9,
-	}
-	operationsTabble = map[string]string{
-		"+": "+",
-		"-": "-",
-		"/": "/",
-		"*": "*",
-		"%": "%",
-	}
-)
-
 const (
 	MATH_OP TokenType = "MATH_OP"
 	INTEGER TokenType = "INTEGER"
 )
 
+var operationFns = map[string]func(l, r int) int{}
+
 func ParseTokens(input string) []Token {
-	tokens := []Token{}
+	var (
+		tokens             = []Token{}
+		buf                = ""
+		prevType TokenType = ""
+	)
 	for i := 0; i < len(input); i++ {
 		v := string(input[i])
+
 		if shouldIgnore(v) {
+			tokens = append(tokens, Token{Type: prevType, Literal: buf})
+			prevType = ""
+			buf = ""
 			continue
 		}
+
 		tt := getTokenType(v)
-		tokens = append(tokens, Token{Type: tt, Literal: v})
+		if prevType == "" {
+			prevType = tt
+		}
+
+		if prevType == tt {
+			buf += v
+		} else {
+			tokens = append(tokens, Token{Type: prevType, Literal: buf})
+			prevType = tt
+			buf = v
+		}
+	}
+	if buf != "" {
+		tokens = append(tokens, Token{Type: prevType, Literal: buf})
 	}
 	return tokens
+}
+
+func BuildEquations(tokens []Token) []Equation {
+	operations := []Equation{}
+	for i := 0; i < len(tokens); i++ {
+		token := tokens[i]
+		if token.Type != INTEGER {
+			fmt.Printf("Equations should start with an integer, but found %s\n", token.Literal)
+		}
+		if i+2 > len(tokens)-1 {
+			fmt.Printf("Incomplete math equation\n")
+		}
+		action := tokens[i+1]
+		right := tokens[i+2]
+		eq := Equation{
+			Left:   token,
+			Action: action,
+			Right:  right,
+		}
+		operations = append(operations, eq)
+		i += 2
+	}
+	return operations
 }
 
 func init() {
@@ -85,19 +111,47 @@ func init() {
 	// }
 }
 
-func ExecOperation(o Operation) int {
-	fn, ok := operationFns[o.Action]
+func ExecOperation(o Equation) int {
+	fn, ok := operationFns[o.Action.Literal]
 	if !ok {
 		log.Panicf("Invalid or unsupported action: %s\n", o.Action)
 	}
-	return fn(o.Left, o.Right)
+	l, _ := strconv.ParseInt(o.Left.Literal, 10, 32)
+	r, _ := strconv.ParseInt(o.Right.Literal, 10, 32)
+	return fn(int(l), int(r))
 }
 
 func getTokenType(s string) TokenType {
-	if _, ok := operationsTabble[s]; ok {
+	switch s {
+	case "+":
 		return MATH_OP
-	}
-	if _, ok := numbersTable[s]; ok {
+	case "-":
+		return MATH_OP
+	case "/":
+		return MATH_OP
+	case "*":
+		return MATH_OP
+	case "%":
+		return MATH_OP
+	case "0":
+		return INTEGER
+	case "1":
+		return INTEGER
+	case "2":
+		return INTEGER
+	case "3":
+		return INTEGER
+	case "4":
+		return INTEGER
+	case "5":
+		return INTEGER
+	case "6":
+		return INTEGER
+	case "7":
+		return INTEGER
+	case "8":
+		return INTEGER
+	case "9":
 		return INTEGER
 	}
 	log.Panicf("Invalid character '%s'\n", s)
@@ -105,17 +159,13 @@ func getTokenType(s string) TokenType {
 }
 
 func shouldIgnore(s string) bool {
-	ignore := false
 	switch s {
 	case "\n":
-		ignore = true
-		break
+		return true
 	case "\n\r":
-		ignore = true
-		break
+		return true
 	case " ":
-		ignore = true
-		break
+		return true
 	}
-	return ignore
+	return false
 }
